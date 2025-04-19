3531238924c16166e713420a4f6104ab6b3856ee
Sai Kumar <kasarla@usc.edu>
2025-04-18 20:07:55 -0400

    Updates

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,jpeg,jpg,png,svg}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
